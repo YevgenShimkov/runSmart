48731f365b23b9f83b8b4dbf2fe120731c39da41
Evgeniy <yakimoto.evgeniy@gmail.com>
2021-07-14 18:45:43 +0300

    add carousel, tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,18 @@
-let reverse = '';
-let prom;
-let s = '123456';
-prom = s.split('');
-console.log(s);
-console.log(prom);
-
-
-for (let i = prom.length - 1; i <= 0; i--) {
-    reverse = reverse + prom[i];
-}
-console.log(reverse);
+$(document).ready(function () {
+  $('.carousel__inner').slick({
+    infinite: true,
+    speed: 1200,
+    // adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-arrow.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/right-arrow.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          dots: true,
+          arrows: false
+        }
+      },
+    ]
+  });
+});
