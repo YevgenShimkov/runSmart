f403f98205e11f9e287183f107e6c4df524d4e5e
Evgeniy <yakimoto.evgeniy@gmail.com>
2021-08-13 20:05:21 +0300

    add dist

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,20 @@ const sass = require('gulp-sass');
 const rename = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+//const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 // Static server
-gulp.task('server', function() {
+gulp.task('server', function () {
     browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
+    gulp.watch("src/*.html").on('change', browserSync.reload)
 });
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
     return gulp.src("src/sass/**/*.+(scss|sass)") // /**/ означает- и все папки, что внутри с тамими же файлами
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(rename({
@@ -23,13 +26,66 @@ gulp.task('styles', function() {
         }))
         .pipe(autoprefixer())
         .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 })
 
-gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'))
-    gulp.watch("src/*.html").on('change', browserSync.reload)
+/**
+ * отслеживание изменений в соответсвующих файлах
+ */
+gulp.task('watch', function () {
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'))
+    gulp.watch("src/*.html").on('change', gulp.parallel('html')); // когда происходят изменения в файле html то будет запускатся задача 'html'
 })
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'))
+/**
+ * уменьшение файла html и перезапись его в dist
+ */
+gulp.task('html', function () {
+    return gulp.src("src/*.html")
+        // получаем файлы по определенному пути
+        .pipe(htmlmin({ collapseWhitespace: true })) // убираем пробелы
+        .pipe(gulp.dest('dist/')) // куда поместится файл после min
+});
+
+/**
+ * перемещаем все файлы из js  в dist
+ */
+gulp.task('scripts', function () {
+    return gulp.src("src/js/**/*.js") // берем любые файлы в папке js  с расширением js
+        .pipe(gulp.dest('dist/js')) // куда поместится файл после min
+});
+
+/**
+ * перемещаем все файлы из fonts  в dist
+ */
+gulp.task('fonts', function () {
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest('dist/fonts')) // куда поместится файл после min
+});
+/**
+ * перемещаем все файлы из icons  в dist
+ */
+gulp.task('icons', function () {
+    return gulp.src("src/icons/**/*")
+        .pipe(gulp.dest('dist/icons')) // куда поместится файл после min
+});
+/**
+ * перемещаем все файлы из mailer  в dist
+ */
+gulp.task('mailer', function () {
+    return gulp.src("src/mailer/**/*")
+        .pipe(gulp.dest('dist/mailer')) // куда поместится файл после min
+});
+
+/**
+ * перемещаем все файлы из images  в dist
+ */
+gulp.task('images', function () {
+    return gulp.src("src/img/**/*")
+     //   .pipe(imagemin()) // обрабатываем все картинки
+        .pipe(gulp.dest('dist/img')) // куда поместится файл после min
+});
+
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'mailer', 'images', 'html'));
